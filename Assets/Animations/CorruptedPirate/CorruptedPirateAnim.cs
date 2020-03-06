9c2c0928b07010ae7e8572a6bd161f28aa1f13ec
FranciscoPires <frelvaspires@gmail.com>
2020-03-06 12:27:03 +0000

    corrupted pirate animado

---
+++ Assets/Animations/CorruptedPirate/CorruptedPirateAnim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorruptedPirateAnim : MonoBehaviour
+{
+    [SerializeField] private Animator AnimController;
+
+    private void Update() {
+        AnimController = GetComponent<Animator>();
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.tag == "Player") {
+            AnimController.Play("shoot");
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.tag == "Player") {
+            AnimController.Play("walk");
+        }
+    }
+}
