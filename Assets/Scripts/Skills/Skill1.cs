b70dab1a0fd9307148163f891a492a2d845559ed
FranciscoPires <frelvaspires@gmail.com>
2020-03-09 17:36:04 +0000

    correr mais rapido (wip)

--- Assets/Scripts/Skills/Skill1.cs
+++ Assets/Scripts/Skills/Skill1.cs
@@ -4,7 +4,37 @@ using UnityEngine;
 
 public class Skill1 : MonoBehaviour
 {
+    [SerializeField] private PlayerController playerinstance;
+    [SerializeField] private float newSpeed;
+    [SerializeField] private SkillsSTM stminstance;
+    [SerializeField] private float skilltime;
+    [SerializeField] private float defaultspeed;
+
+    private void Update() {
+        playerinstance.GetComponent<PlayerController>();
+        stminstance.GetComponent<SkillsSTM>();
+    }
     public void Skill1method() {
         Debug.Log("skill1");
+        StartCoroutine(SkillTime());
+        //playerinstance.MoveSpeed = newSpeed;
+    }
+
+
+    IEnumerator SkillTime() {
+        // stminstance.skills = SkillsSTM.Skills.noskill;
+
+        float timePassed = 0; 
+        while(timePassed < skilltime)
+        {
+            playerinstance.MoveSpeed = newSpeed;
+            timePassed += Time.deltaTime;
+            //yield return stminstance.skills = SkillsSTM.Skills.noskill;
+            yield return null;
+            //yield return playerinstance.MoveSpeed;
+            playerinstance.MoveSpeed = defaultspeed;
+            //yield return null;
+
+        }
     }
 }
