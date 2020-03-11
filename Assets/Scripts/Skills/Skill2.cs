c83b810bc276e4c514f832aa460e6326f68f02e3
FranciscoPires <frelvaspires@gmail.com>
2020-03-11 15:08:13 +0000

    skill: aumentar o dano

--- Assets/Scripts/Skills/Skill2.cs
+++ Assets/Scripts/Skills/Skill2.cs
@@ -4,7 +4,30 @@ using UnityEngine;
 
 public class Skill2 : MonoBehaviour
 {
+    [SerializeField] private PlayerController playerinstance;
+    [SerializeField] private float newDamage;
+    [SerializeField] private SkillsSTM stminstance;
+    [SerializeField] private float skilltime;
+    [SerializeField] private float defaultDamage;
+
+    private void Update() {
+        playerinstance.GetComponent<PlayerController>();
+        stminstance.GetComponent<SkillsSTM>();
+    }
     public void Skill2method() {
-        Debug.Log("skill2");
+        StartCoroutine(SkillTime());
+        playerinstance.AttackDamage = newDamage;
+    }
+
+    IEnumerator SkillTime() {
+        float timePassed = 0;
+        while (timePassed < skilltime) {
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
+        if (timePassed >= skilltime) {
+            playerinstance.AttackDamage = defaultDamage;
+            stminstance.skills = SkillsSTM.Skills.noskill;
+        }
     }
 }
