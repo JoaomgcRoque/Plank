ce8425021c4bc5eca04c529b937e10f0debf3e0e
FranciscoPires <frelvaspires@gmail.com>
2020-03-12 14:27:37 +0000

    granada - wip

---
+++ Assets/Scripts/Skills/Grenade.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    [SerializeField] private PlayerController PlayerCont;
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private Transform lefthand;
+    [SerializeField] private float force;
+    [SerializeField] private GameObject grenade;
+    [SerializeField] private float secondstoexplode;
+    [SerializeField] private bool doeskill = false;
+
+    private void Start() {
+        PlayerCont = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+        lefthand = GameObject.Find("left_hand").GetComponent<Transform>();
+        rigidbody.AddForce(lefthand.transform.forward * force);
+
+    }
+
+    private void Update() {
+        StartCoroutine(Countdown(secondstoexplode));
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.tag == "Enemy" && doeskill == true) {
+            Destroy(grenade);
+            Destroy(other);
+            Debug.Log("Explosion");
+            doeskill = false;
+        }
+
+        if (other.tag == "Player" && doeskill == true) {
+            Destroy(grenade);
+            Destroy(other);
+            Debug.Log("Explosion");
+            doeskill = false;
+        }
+    }
+
+    IEnumerator Countdown(float secondstoexplode) {
+        yield return new WaitForSeconds(secondstoexplode);
+        doeskill = true;
+        Debug.Log("Explosion");
+    }
+}
--- Assets/Scripts/Skills/Skill3.cs
+++ Assets/Scripts/Skills/Skill3.cs
@@ -4,7 +4,36 @@ using UnityEngine;
 
 public class Skill3 : MonoBehaviour
 {
-    public void Skill3method() {
-        Debug.Log("skill3");
+    [SerializeField] private PlayerController playerinstance;
+    [SerializeField] private SkillsSTM stminstance;
+    [SerializeField] private GameObject bomb;
+    [SerializeField] private float skilltime;
+    [SerializeField] private bool canThrow = true;
+    [SerializeField] private Transform hand;
+
+    private void Update() {
+        playerinstance.GetComponent<PlayerController>();
+        stminstance.GetComponent<SkillsSTM>();
     }
-}
+
+    public void Skill3method() {
+        StartCoroutine(SkillTime());
+        Debug.Log("skill3");
+        Throw();
+    }
+
+    private void Throw() {
+        if(canThrow == true) {
+            Debug.Log("Throw");
+            Instantiate(bomb, hand.transform.position, hand.transform.rotation);
+            canThrow = false;
+        }
+    }
+
+    IEnumerator SkillTime() {
+            yield return new WaitForSeconds(skilltime);
+            stminstance.skills = SkillsSTM.Skills.noskill;
+            canThrow = true;
+    }
+ }
+
