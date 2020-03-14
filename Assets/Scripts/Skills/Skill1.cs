3d6ed9954c5c102e7f058edac342e03b91d693d1
FranciscoPires <frelvaspires@gmail.com>
2020-03-14 19:55:18 +0000

    limpeza de codigo morto no skill 1
    tentei corrigir o pequeno bug que tem (sem sucesso) e limpei codigo morto

--- Assets/Scripts/Skills/Skill1.cs
+++ Assets/Scripts/Skills/Skill1.cs
@@ -9,19 +9,12 @@ public class Skill1 : MonoBehaviour
     [SerializeField] private SkillsSTM stminstance;
     [SerializeField] public float skilltime;
     [SerializeField] private float defaultspeed;
-    [SerializeField] private bool isSpeed;
 
     public float deletethis;
 
     private void Update() {
         playerinstance.GetComponent<PlayerController>();
         stminstance.GetComponent<SkillsSTM>();
-        if(isSpeed == true) {
-            playerinstance.MoveSpeed = newSpeed;
-        }
-        if(isSpeed == false) {
-            playerinstance.MoveSpeed = defaultspeed;
-        }
     }
     public void Skill1method() {
         StartCoroutine(SkillTime());
@@ -31,19 +24,17 @@ public class Skill1 : MonoBehaviour
        float timePassed = 0; 
        while(timePassed < skilltime)
        {
-            isSpeed = true;
             timePassed += Time.deltaTime;
-            //playerinstance.MoveSpeed = newSpeed;
+            playerinstance.MoveSpeed = newSpeed;
             deletethis = timePassed;
             yield return null;
-        }
+       }
        if(timePassed >= skilltime) {
-            //playerinstance.MoveSpeed = defaultspeed;
+            playerinstance.MoveSpeed = defaultspeed;
             timePassed = skilltime;
-            isSpeed = false;
             //stminstance.skills = SkillsSTM.Skills.noskill;
             //yield return null;
-        }
+       }
     }
 
 }
