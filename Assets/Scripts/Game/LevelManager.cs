f2f0e4bd5491ddde8d9f8b91817cc1aefd8485d9
FranciscoPires <frelvaspires@gmail.com>
2020-03-23 14:27:21 +0000

    win island feito

---
+++ Assets/Scripts/Game/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public int numberofdead;
+    [SerializeField] private int maxdead;
+    [SerializeField] private GameObject WinMenu;
+
+    private void Awake() {
+        WinMenu.SetActive(false);
+    }
+    private void Update() {
+        if (numberofdead == maxdead) {
+            Win();
+        }
+    }
+
+    private void Win() {
+        WinMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void NextIsland() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /*public void Pause() {
+        PauseCanvas.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true;
+    }*/
+}
