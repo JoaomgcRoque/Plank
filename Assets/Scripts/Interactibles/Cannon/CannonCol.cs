b549e2369f8aed0c1f76a39e184eb9cac032f24d
FranciscoPires <frelvaspires@gmail.com>
2020-04-14 11:45:43 +0100

    canhao terminado

--- Assets/Scripts/Interactibles/Cannon/CannonCol.cs
+++ Assets/Scripts/Interactibles/Cannon/CannonCol.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class CannonCol : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private CannonSTM cannonstm;
+    [SerializeField] private SwordAttack swordattack;
+    public bool isOpen = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.tag == "Player" && swordattack.Attacking == true
+            && isOpen == false) {
+            cannonstm.states = CannonSTM.States.shoot;
+            isOpen = true;
+        }
     }
 }
