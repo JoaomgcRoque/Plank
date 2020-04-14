1ac4e1fa96a8afbb330395b29907994e1af51221
FranciscoPires <frelvaspires@gmail.com>
2020-04-14 11:41:10 +0100

    canhao implementado

---
+++ Assets/Scripts/Interactibles/Cannon/CannonBullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBullet : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private Transform Allign;
+    [SerializeField] private float force;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float bulletTimeLife;
+
+
+    private void Start() {
+        Allign = GetComponentInParent<Transform>();
+        rigidbody.AddForce(Allign.transform.forward * force);
+        StartCoroutine(BulletDestruction(bulletTimeLife));
+    }
+
+    private void Update() {
+        Allign = GameObject.Find("shoot").transform;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Enemy") {
+            Destroy(bullet);
+            other.GetComponent<EnemyController>().Health = 0f;
+        }
+
+        if (other.tag == "Player") {
+            Destroy(bullet);
+            other.GetComponent<PlayerController>().Health = 0f;
+        }
+
+        if (other.tag == "Obstacle") {
+            Destroy(bullet);
+        }
+    }
+
+    IEnumerator BulletDestruction(float bulletTimeLife) {
+        yield return new WaitForSeconds(bulletTimeLife);
+        Destroy(bullet);
+        StopCoroutine(BulletDestruction(bulletTimeLife));
+    }
+}
---
+++ Assets/Scripts/Interactibles/Cannon/CannonSTM.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonSTM : MonoBehaviour
+{
+    public enum States { idle, shoot, off }
+    public States states;
+
+    [SerializeField] private GameObject smoke;
+    [SerializeField] private float time;
+    [SerializeField] private float smoketime;
+    [SerializeField] private GameObject cannonBullet;
+    [SerializeField] private Transform shoot;
+    [SerializeField] private bool hasShoot = false;
+
+    private void Update() {
+        STM();
+    }
+
+    private void STM() {
+        switch (states) {
+            case States.idle:
+                smoke.SetActive(false);
+                break;
+            case States.shoot:
+                StartCoroutine(TimetoShoot(time));
+                break;
+            case States.off:
+                StopCoroutine(TimetoShoot(time));
+                StartCoroutine(SmokeTime(smoketime));
+                break;
+        }
+    }
+
+    private void Shoot() {
+        if (hasShoot == false) {
+            hasShoot = true;
+            Instantiate(cannonBullet, shoot.transform.position, shoot.transform.rotation);
+        }
+    }
+
+    IEnumerator TimetoShoot(float time) {
+        yield return new WaitForSeconds(time);
+        Shoot();
+        states = States.off;
+    }
+
+    IEnumerator SmokeTime(float smoketime) {
+        smoke.SetActive(true);
+        yield return new WaitForSeconds(smoketime);
+        smoke.SetActive(false);
+        StopCoroutine(SmokeTime(smoketime));
+    }
+}
