618e9ec9456f43a69e7736489c101d64f2278e77
FranciscoPires <frelvaspires@gmail.com>
2020-05-11 19:25:36 +0100

    island 4 terminado + colliders

---
+++ Assets/Scripts/Game/TriggerWave.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerWave : MonoBehaviour
+{
+    [SerializeField] private bool isWave;
+    [SerializeField] private GameObject wave;
+
+    private void Awake() {
+        isWave = false;
+        wave.SetActive(false);
+    }
+
+    private void Update() {
+        if(isWave == true) {
+            wave.SetActive(true);
+        }
+    }
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            isWave = true;
+        }
+    }
+}
