89f5bd52fd48fee7910ff81845816e22e28380af
FranciscoPires <frelvaspires@gmail.com>
2020-05-28 11:09:54 +0100

    ajustei melhor os spawns da ilha 4 e melhorei o sistema da wave

---
+++ Assets/Scripts/Game/InvisibleDoor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleDoor : MonoBehaviour
+{
+    [SerializeField] private GameObject invisibledoor;
+    [SerializeField] private LevelManager levelmanager;
+    [SerializeField] private int killnumber;
+
+    private void Start() {
+        invisibledoor.SetActive(true);
+    }
+
+    private void Update() {
+        levelmanager = GameObject.Find("GameManager").GetComponent<LevelManager>();
+        if (levelmanager.numberofdead == killnumber) {
+            invisibledoor.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Game/TriggerWave.cs
+++ Assets/Scripts/Game/TriggerWave.cs
@@ -6,15 +6,21 @@ public class TriggerWave : MonoBehaviour
 {
     [SerializeField] private bool isWave;
     [SerializeField] private GameObject wave;
+    [SerializeField] private GameObject invisibledoor2;
 
     private void Awake() {
         isWave = false;
         wave.SetActive(false);
     }
 
+    private void Start() {
+        invisibledoor2.SetActive(false);
+    }
+
     private void Update() {
         if(isWave == true) {
             wave.SetActive(true);
+            invisibledoor2.SetActive(true);
         }
     }
     private void OnTriggerEnter(Collider other) {
