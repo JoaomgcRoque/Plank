8c96badea3b29a931cda857f61cc469aba129dab
Nuno <nunosilvares@hotmail.com>
2020-06-06 13:32:49 +0100

    Boss Waves and some fixes I guess

--- Assets/Scripts/Boss Boat/Controller.cs
+++ Assets/Scripts/Boss Boat/Controller.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
     public Animator animator, cannon1, cannon2, cannon3;
+    public Canvas canvas;
     public GameObject Win;
-    public bool fase1, fase2, fase3, fase4, stop = false;
-    public int nAttacksFase1, nAttacksFase2, nAttacksFase3;
+    public GameObject[] wave1Enemies, wave2Enemies, wave3Enemies;
+    public bool fase1, fase2, fase3, fase4, wave1, wave2, wave3, stop = false;
+    public int nAttacksFase1, nAttacksFase2, nAttacksFase3, counter;
 
     void Update()
     {
@@ -35,7 +39,33 @@ public class Controller : MonoBehaviour
                 animator.SetInteger("Attack", 0);
                 yield return new WaitForSeconds(0.5f);
             }
-            cannon1.SetBool("Open", true);
+        }
+        BossWave1();
+    }
+
+    public void BossWave1()
+    {
+        stop = false;
+        animator.SetInteger("Wave", 1);
+        if (wave1)
+        {
+            counter = 0;
+            for (int x = 0; x < wave1Enemies.Length; x++)
+                if (wave1Enemies[x].GetComponent<NavMeshAgent>() == null)
+                    counter++;
+
+            if (counter == wave1Enemies.Length)
+            {
+                cannon1.SetBool("Open", true);
+                for (int x = 0; x < wave1Enemies.Length; x++)
+                {
+                    Destroy(canvas.transform.GetChild(6+x).gameObject);
+                    Destroy(wave1Enemies[x]);
+                }
+                wave2 = true;
+                stop = true;
+                wave1 = false;
+            }
         }
     }
 
@@ -53,7 +83,33 @@ public class Controller : MonoBehaviour
                 animator.SetInteger("Attack", 0);
                 yield return new WaitForSeconds(0.5f);
             }
-            cannon2.SetBool("Open", true);
+        }
+        BossWave2();
+    }
+
+    public void BossWave2()
+    {
+        stop = false;
+        animator.SetInteger("Wave", 2);
+        if (wave2)
+        {
+            counter = 0;
+            for (int x = 0; x < wave2Enemies.Length; x++)
+                if (wave2Enemies[x].GetComponent<NavMeshAgent>() == null)
+                    counter++;
+
+            if (counter == wave2Enemies.Length)
+            {
+                cannon2.SetBool("Open", true);
+                for (int x = 0; x < wave2Enemies.Length; x++)
+                {
+                    Destroy(canvas.transform.GetChild(6 + x).gameObject);
+                    Destroy(wave2Enemies[x]);
+                }
+                wave3 = true;
+                stop = true;
+                wave2 = false;
+            }
         }
     }
 
@@ -71,7 +127,31 @@ public class Controller : MonoBehaviour
                 animator.SetInteger("Attack", 0);
                 yield return new WaitForSeconds(0.5f);
             }
-            cannon3.SetBool("Open", true);
+        }
+        BossWave3();
+    }
+    
+    public void BossWave3()
+    {
+        stop = false;
+        animator.SetInteger("Wave", 3);
+        if (wave3)
+        {
+            counter = 0;
+            for (int x = 0; x < wave3Enemies.Length; x++)
+                if (wave3Enemies[x].GetComponent<NavMeshAgent>() == null)
+                    counter++;
+
+            if (counter == wave3Enemies.Length)
+            {
+                cannon3.SetBool("Open", true);
+                for (int x = 0; x < wave3Enemies.Length; x++)
+                {
+                    Destroy(canvas.transform.GetChild(6 + x).gameObject);
+                    Destroy(wave3Enemies[x]);
+                }
+                stop = true;
+            }
         }
     }
 
