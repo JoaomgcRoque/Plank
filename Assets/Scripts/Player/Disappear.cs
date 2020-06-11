214970d3b91947bc321b3a9e69cc66b0f2863618
FranciscoPires <frelvaspires@gmail.com>
2020-06-11 17:24:34 +0100

    pequena correcao

--- Assets/Scripts/Player/Disappear.cs
+++ Assets/Scripts/Player/Disappear.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class Disappear : MonoBehaviour
 {
     [SerializeField] private float destroytime;
-    [SerializeField] private Renderer rend;
     public GameObject footstep;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(Destroy(destroytime));
     }
-
 
     IEnumerator Destroy(float destroytime) {
             yield return new WaitForSeconds(destroytime);
